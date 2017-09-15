49ab2fd0aca96381ccf9157de2392516ca4219b2
silviojof <silviojof@gmail.com>
2017-09-14 20:32:42 -0700

    Ad GET /users/me and authetication with middleware

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ const _ = require('lodash');
 const {mongoose} = require('./db/mongoose');
 const {Todo} = require('./models/todo');
 const {User} = require('./models/user');
+const {authenticate} = require('./middleware/authenticate');
 
 const app = express();
 const port = process.env.PORT;
@@ -110,6 +111,12 @@ app.post('/users', (req, res) => {
   }).catch((e) => {
     res.status(400).send(e);
   });
+});
+
+
+
+app.get('/users/me', authenticate, (req, res) => {
+  res.send(req.user);
 });
 
 app.listen(port, () => {
